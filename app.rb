5a4d7d93f9d2935877f2f4a66e792b3fbfbf4844
Frank Rosales <frank.tipti.market>
2020-03-06 12:01:45 -0500

    done

--- app.rb
+++ app.rb
@@ -10,5 +10,9 @@ end
 post "/guardar" do
   session['word'] = ''
   session['word'] += params["texto"]
+  hang_plus = HangPlus.new
+  result = hang_plus.validar params["texto"]
+  session['salida'] = "#{result} incorrecto"
+  session['correct_letter'] = "#{result}"
   erb(:index)
 end
--- lib/hangplus.rb
+++ lib/hangplus.rb
@@ -2,4 +2,8 @@ class HangPlus
   def initialize word = "arbol"
     @word = word
   end
+
+  def validar letter
+    letter
+  end
 end
